46e01ef3f15d401cc358ab9840db2ad6713da36c
Peter Levinsky <PELE@zealand.dk>
2023-02-08 12:50:59 +0100

    Deleted working

--- ClassDemoPizzaApp/Pages/Pizzas/Delete.cshtml.cs
+++ ClassDemoPizzaApp/Pages/Pizzas/Delete.cshtml.cs
@@ -1,12 +1,43 @@
+using ClassDemoPizzaApp.services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaLib.model;
 
 namespace ClassDemoPizzaApp.Pages.Pizzas
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        /*
+         * modtage dependency injection
+         */
+        private IPizzaRepositoryService _service;
+
+        public DeleteModel(IPizzaRepositoryService service)
         {
+            _service = service;
+        }
+
+
+        /*
+         * Properties til viewet
+         */
+
+        public Pizza Pizza { get; set; }
+
+        public void OnGet(int id)
+        {
+            Pizza = _service.GetById(id);
+        }
+
+        public IActionResult OnPostSlet(int id)
+        {
+            _service.Delete(id);
+            return RedirectToPage("Index");
+        }
+
+        public IActionResult OnPostFortryd()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
