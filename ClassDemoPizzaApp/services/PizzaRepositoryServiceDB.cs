495d12aaf277681b404a028fdf772f906290abf9
Peter Levinsky <PELE@zealand.dk>
2023-03-15 10:02:23 +0100

    Alle metoder implementeret

--- ClassDemoPizzaApp/services/PizzaRepositoryServiceDB.cs
+++ ClassDemoPizzaApp/services/PizzaRepositoryServiceDB.cs
@@ -1,4 +1,5 @@
 ﻿using PizzaLib.model;
+using System;
 using System.Data.SqlClient;
 
 namespace ClassDemoPizzaApp.services
@@ -9,12 +10,59 @@ namespace ClassDemoPizzaApp.services
 
         public Pizza Create(Pizza pizza)
         {
-            throw new NotImplementedException();
+            String sql = "insert into Pizza values(@Name, @Pris)";
+
+            // forbindelse
+            SqlConnection conn = new SqlConnection(Secret.GetConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            //cmd.Parameters.AddWithValue("@ID", pizza.Id); // blliver lavet i databasen
+            cmd.Parameters.AddWithValue("@Name", pizza.Name);
+            cmd.Parameters.AddWithValue("@Pris", pizza.Pris);
+            
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row == 1)
+            {
+                return pizza;
+            }
+            else
+            {
+                return null; // eller exception
+            }
         }
 
         public Pizza Delete(int id)
         {
-            throw new NotImplementedException();
+            // finder først personen
+            Pizza p = GetById(id);
+            if (p is null)
+            {
+                return null;
+            }
+
+            String sql = "delete from Pizza where Id = @ID";
+
+            // forbindelse
+            SqlConnection conn = new SqlConnection(Secret.GetConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row == 1)
+            {
+                return p;
+            }
+            else
+            {
+                return null; // eller exception
+            }
         }
 
         public List<Pizza> GetAll()
@@ -50,12 +98,50 @@ namespace ClassDemoPizzaApp.services
 
         public Pizza GetById(int id)
         {
-            throw new NotImplementedException();
+            SqlConnection conn = new SqlConnection(Secret.GetConnectionString);
+            conn.Open();
+
+            String sql = "select * from Pizza where Id = @ID";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            
+            if (reader.Read())
+            {
+                return ReadPizza(reader);
+
+            }
+
+            throw new KeyNotFoundException();
         }
 
         public Pizza Update(int id, Pizza pizza)
         {
-            throw new NotImplementedException();
+            String sql = "update Pizza set Name=@Name, Price=@Pris where Id = @ID";
+
+            // forbindelse
+            SqlConnection conn = new SqlConnection(Secret.GetConnectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ID", id);
+            cmd.Parameters.AddWithValue("@Name", pizza.Name);
+            cmd.Parameters.AddWithValue("@Pris", pizza.Pris);
+
+
+            int row = cmd.ExecuteNonQuery();
+
+            if (row == 1)
+            {
+                pizza.Id = id;
+                return pizza;
+            }
+            else
+            {
+                return null; // eller exception
+            }
         }
     }
 }
